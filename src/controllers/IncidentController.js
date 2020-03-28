b5941bbc6c221598ef12b3e1c2482d17f6949c6f
Brenner L. Oliveira <brennerlimaoliveira@gmail.com>
2020-03-27 22:28:21 -0300

    Implementa listagem de incidentes

--- src/controllers/IncidentController.js
+++ src/controllers/IncidentController.js
@@ -1,6 +1,12 @@
 const connection = require('../database/connection');
 
 module.exports = {
+  async index(request, response) {
+    const incidents = await connection('incidents').select('*');
+
+    return response.json(incidents);
+  },
+
   async create(request, response) {
     const ong_id = request.headers.authorization;
     const { title, description, value } = request.body;
